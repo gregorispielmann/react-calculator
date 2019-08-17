99e70578b806fff1cfc1ce2be027af51f3dd8480
Gregori Spielmann <gregorispielmann@gmail.com>
2019-08-17 16:00:22 -0300

    Updates

---
+++ precache-manifest.a78ae21b266f6026746ca92389acc5c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f198148354310483520ed3d344e4a890",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "089d6eac53e11d753f7d",
+    "url": "/react-calculator/static/css/main.cf59e2d5.chunk.css"
+  },
+  {
+    "revision": "4c476b1575083aaec8f1",
+    "url": "/react-calculator/static/js/2.344b7583.chunk.js"
+  },
+  {
+    "revision": "089d6eac53e11d753f7d",
+    "url": "/react-calculator/static/js/main.c4385760.chunk.js"
+  },
+  {
+    "revision": "2c7460d17796b892bd56",
+    "url": "/react-calculator/static/js/runtime~main.a948531e.js"
+  },
+  {
+    "revision": "7c13b04382bb3c4a6a50211300a1b072",
+    "url": "/react-calculator/static/media/RobotoMono-Bold.7c13b043.ttf"
+  }
+]);
